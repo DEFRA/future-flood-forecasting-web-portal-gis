b010103b6e6fe5dc13d8370af5e9b2826d770a9f
Jack Sheppard <a-jack.sheppard@defracloud.onmicrosoft.com>
2020-05-11 10:04:32 +0100

    fixed typo in Octopus bootstrap script

--- bootstrap_octopus_deploy.sh
+++ bootstrap_octopus_deploy.sh
@@ -7,7 +7,7 @@ export FFFS_WEB_PORTAL_GIS_DB_DATA_USERNAME='#{webPortalGisDbDataUsername}'
 export FFFS_WEB_PORTAL_GIS_DB_DATA_PASSWORD='#{webPortalGisDbDataPassword}'
 export FFFS_WEB_PORTAL_GIS_DB_READER_USERNAME='#{webPortalGisDbReaderUsername}'
 export FFFS_WEB_PORTAL_GIS_DB_READER_PASSWORD='#{webPortalGisDbReaderPassword}'
-export FFFS_WEB_PORTAL_GIS_SDE_PASSWORD='#{webPortalGisDbSdePassword}'
+export FFFS_WEB_PORTAL_GIS_DB_SDE_PASSWORD='#{webPortalGisDbSdePassword}'
 
 echo "Executing Maven DB script"
 mvn clean process-resources
